72b470f10aa8694a070968c7f84560c47d87ce54
maykonmichel <maykonmichelpalma@gmail.com>
2020-06-23 08:40:37 -0300

    Added cyclical coordinates screen

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Header from './components/Header';
+import CyclicalCoordinates from './pages/CyclicalCoordinates';
 import Home from './pages/Home';
 import Newton from './pages/Newton';
 
@@ -14,6 +15,9 @@ export default () => (
       </Route>
       <Route path="/newton" exact>
         <Newton />
+      </Route>
+      <Route path="/coordenadas-ciclicas" exact>
+        <CyclicalCoordinates />
       </Route>
     </Switch>
   </BrowserRouter>
